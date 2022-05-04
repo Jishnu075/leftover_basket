bed8fde4440eb3e006326eb7fda15b165c2bcceb
Jishnu Biju <jishnubiju075@gmail.com>
2022-05-04 21:36:10 +0530

    isloggedIn based pages

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:leftover_basket/firebase/auth_service.dart';
 import 'package:leftover_basket/screens/main_donar_view.dart';
-import 'package:leftover_basket/screens/user_selection_screen.dart';
+import 'package:leftover_basket/screens/onboarding_screen.dart';
 import 'package:provider/provider.dart';
 import 'models/user_model.dart';
 
 class Wrapper extends StatelessWidget {
+  static const String id = 'wrapper';
+
   const Wrapper({Key? key}) : super(key: key);
 
   @override
@@ -16,7 +18,7 @@ class Wrapper extends StatelessWidget {
         builder: (_, AsyncSnapshot<User?> snapshot) {
           if (snapshot.connectionState == ConnectionState.active) {
             final User? user = snapshot.data;
-            return user == null ? UserSelectionScreen() : MainDonorView();
+            return user == null ? OnBoardingScreen() : MainDonorView();
           } else {
             return Scaffold(body: Center(child: CircularProgressIndicator()));
           }
